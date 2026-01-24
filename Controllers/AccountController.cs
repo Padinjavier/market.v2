f4d220c5ad433f2444c73a6fc883c59a106627b9
Padinjavier <javierpadin661@mail.com>
2026-01-24 01:56:10 -0500

    cambiar algoritmo de hash de contraseña a SHA256

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -63,12 +63,13 @@ namespace market.Controllers
 
         private string HashPassword(string password)
         {
-            using (var sha1 = SHA1.Create())
+            using (var sha256 = SHA256.Create())
             {
                 var bytes = Encoding.UTF8.GetBytes(password);
-                var hash = sha1.ComputeHash(bytes);
+                var hash = sha256.ComputeHash(bytes);
                 return BitConverter.ToString(hash).Replace("-", "").ToLower();
             }
         }
+
     }
 }
